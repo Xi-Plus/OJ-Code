c3f1f7adf5208e8c680f8f032d589e41fe88bb45
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-02 23:24:23 +0800

    tioj 1754 AC

--- TIOJ/tioj 1754.cpp
+++ TIOJ/tioj 1754.cpp
@@ -5,16 +5,18 @@ using namespace std;
 int n,m,k;
 long long dp[1010][1010];
 const long long mod=1000000007;
-long long dfs(int step,int x_step){
+long long dfs(int step,int x_step,bool start){
 	if(dp[step][x_step]!=-1) return dp[step][x_step];
+	if(step<=x_step) return 0;
 	if(step==1){
 		if(x_step==0)return 1;
 		else return 0;
 	}
 	if(x_step==0){
-		return dp[step][x_step]=((n-k-1)*dfs(step-1,0)%mod+dfs(step-1,k))%mod;
+		return dp[step][x_step]=((n-k-1)*dfs(step-1,0,0)%mod+dfs(step-1,k,0))%mod;
 	}else {
-		return dp[step][x_step]=(n-k+x_step-1)*dfs(step-1,x_step-1)%mod;
+		if(start)return dp[step][x_step]=(n-k+x_step-1)*dfs(step-1,x_step-1,1)%mod;
+		else return dp[step][x_step]=(n-k)*dfs(step-1,x_step-1,0)%mod;
 	}
 }
 int main(){
@@ -24,6 +26,6 @@ int main(){
 	while(t--){
 		cin>>n>>m>>k;
 		memset(dp,-1,sizeof(dp));
-		cout<<dfs(m,k)%mod<<endl;
+		cout<<dfs(m,k,1)%mod<<endl;
 	}
 }
