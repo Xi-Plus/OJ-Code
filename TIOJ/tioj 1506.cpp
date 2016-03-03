322f4b22cfdeebf41a9e10d0ae261286e90d03f4
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-03 12:21:38 +0800

    tioj 1506 AC fix

--- TIOJ/tioj 1506.cpp
+++ TIOJ/tioj 1506.cpp
@@ -10,7 +10,7 @@ Multiple Cramer(double a,double b,double c,double d,double e,double f){
 	// cx+dy=f
 	// https://zh.wikipedia.org/wiki/%E5%85%8B%E8%90%8A%E5%A7%86%E6%B3%95%E5%89%87#.E4.BE.8B.E5.AD.90
 	if(a*d==b*c)return {NAN,NAN};
-	return {(e*d-b*f)/(a*d-b*c),(a*f-e*c)/(a*d-b*c)};
+	return {(b*f-e*d)/(b*c-a*d)+0,(e*c-a*f)/(b*c-a*d)+0};
 }
 int main(){
 //	ios::sync_with_stdio(false);
@@ -21,4 +21,3 @@ int main(){
 		cout<<fixed<<setprecision(2)<<t.x<<endl<<t.y<<endl;
 	}
 }
-
