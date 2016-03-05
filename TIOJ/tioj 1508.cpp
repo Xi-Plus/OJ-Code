33af7eceb8cdf8a4a38e9f552faf55d2c03ff127
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-05 11:24:10 +0800

    tioj 1508 AC better

--- TIOJ/tioj 1508.cpp
+++ TIOJ/tioj 1508.cpp
@@ -6,24 +6,28 @@ bool dp[10110];
 int main(){
 //	ios::sync_with_stdio(false);
 //	cin.tie(0);
-	int m,n,t;
+	int m,n;
 	cin>>m>>n;
 	while(m--){
 		int halfsum=0;
-		memset(dp,0,sizeof(dp));
-		dp[0]=1;
+		int v[n];
 		for(int q=0;q<n;q++){
-			cin>>t;
-			for(int w=10000;w>=0;w--){
-				dp[w+t]=max(dp[w+t],dp[w]);
-			}
-			halfsum+=t;
+			cin>>v[q];
+			halfsum+=v[q];
 		}
 		if(halfsum%2){
 			cout<<"No"<<endl;
 			continue;
 		}
 		halfsum/=2;
+		memset(dp,0,sizeof(dp));
+		dp[0]=1;
+		for(int q=0;q<n;q++){
+			for(int w=10000;w>=0;w--){
+				dp[w+v[q]]=max(dp[w+v[q]],dp[w]);
+			}
+			if(dp[halfsum]) break;
+		}
 		if(dp[halfsum]){
 			cout<<"Yes"<<endl;
 		}else {
