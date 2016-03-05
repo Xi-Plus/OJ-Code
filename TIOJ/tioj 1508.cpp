7f8fa4024d5d3aed0e02a634e5326cefaf8405fe
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-05 11:20:29 +0800

    tioj 1508 AC

--- TIOJ/tioj 1508.cpp
+++ TIOJ/tioj 1508.cpp
@@ -2,44 +2,29 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 using namespace std;
-int n;
-int v[100];
-int halfsum=0;
-bool ans=0;
-void dfs(int index,int suma,int sumb){
-	if(ans)return ;
-	if(suma==halfsum||sumb==halfsum){
-		ans=1;
-		return ;
-	}
-	else if(suma>halfsum||sumb>halfsum) return;
-	if(index>=n) return;
-	dfs(index+1,suma+v[index],sumb);
-	dfs(index+1,suma,sumb+v[index]);
-}
-bool cmp(int a,int b){
-	return a>b;
-}
+bool dp[10110];
 int main(){
 //	ios::sync_with_stdio(false);
 //	cin.tie(0);
-	int m;
+	int m,n,t;
 	cin>>m>>n;
 	while(m--){
-		halfsum=0;
-		ans=0;
+		int halfsum=0;
+		memset(dp,0,sizeof(dp));
+		dp[0]=1;
 		for(int q=0;q<n;q++){
-			cin>>v[q];
-			halfsum+=v[q];
+			cin>>t;
+			for(int w=10000;w>=0;w--){
+				dp[w+t]=max(dp[w+t],dp[w]);
+			}
+			halfsum+=t;
 		}
 		if(halfsum%2){
 			cout<<"No"<<endl;
 			continue;
 		}
 		halfsum/=2;
-		sort(v,v+n,cmp);
-		dfs(1,0,0);
-		if(ans){
+		if(dp[halfsum]){
 			cout<<"Yes"<<endl;
 		}else {
 			cout<<"No"<<endl;
