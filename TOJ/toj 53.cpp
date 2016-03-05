56329b7a718f35942b81e934e61639cb02f08d3d
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-05 13:20:55 +0800

    toj 53 AC se

--- TOJ/toj 53.cpp
+++ TOJ/toj 53.cpp
@@ -1,15 +1,16 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cstdlib>
-#include<sstream>
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
 using namespace std;
 int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
     string s;
     while(getline(cin,s)){
         int ans=0;
         int a;
-        istringstream t(s);
+        stringstream t;
+        t<<s;
         while(t>>a)ans+=a;
         cout<<ans<<endl;
     }
