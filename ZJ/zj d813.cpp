c9e638b305983aed1fec639007fff615e3a46ea8
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-14 15:53:20 +0800

    zj d813 AC

---
+++ ZJ/zj d813.cpp
@@ -0,0 +1,35 @@
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int v[50010];
+int find(int n){
+	if(n==v[n])return n;
+	return v[n]=find(v[n]);
+}
+bool bfs(int a,int b){
+	a=find(a);
+	b=find(b);
+	v[b]=a;
+	if(a==b)return 0;
+	return 1;
+}
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	int n,m;
+	int cas=0;
+	while(cin>>n>>m){
+		if(n==0&&m==0)break;
+		for(int q=1;q<=n;q++){
+			v[q]=q;
+		}
+		int ans=n;
+		while(m--){
+			int a,b;
+			cin>>a>>b;
+			ans-=bfs(a,b);
+		}
+		cout<<"Case "<<++cas<<": "<<ans<<endl;
+	}
+}
