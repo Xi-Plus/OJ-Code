c98fe6677d69a8cc55c1f8174e71eb329e3fc13e
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-14 15:24:25 +0800

    zj d636 AC

---
+++ ZJ/zj d636.cpp
@@ -0,0 +1,22 @@
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+const long long m=10007;
+long long power(long long a,long long b){
+	if(b==1) return a;
+	if(b&1){
+		return power(a,b/2)%m*power(a,b/2)%m*a%m;
+	}else {
+		return power(a,b/2)%m*power(a,b/2)%m;
+	}
+}
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	long long a,b;
+	while(cin>>a>>b){
+		cout<<power(a,b)<<endl;
+	}
+}
+
