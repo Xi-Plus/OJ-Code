4fb768639ec4ea2dde9adc0c06c6c67fbde3f7de
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-14 23:21:59 +0800

    uva 272 AC

---
+++ UVa/uva 272.cpp
@@ -0,0 +1,23 @@
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	string s;
+	bool x=1;
+	while(getline(cin,s)){
+		int sz=s.size();
+		for(int q=0;q<sz;q++){
+			if(s[q]=='"'){
+				if(x)cout<<"``";
+				else cout<<"''";
+				x^=1;
+			}else {
+				cout<<s[q];
+			}
+		}
+		cout<<endl;
+	}
+}
