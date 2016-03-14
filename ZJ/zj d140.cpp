62e488a2fcd79a07502f047fab31eef8e27595ba
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-14 22:54:34 +0800

    zj d140 AC

---
+++ ZJ/zj d140.cpp
@@ -0,0 +1,22 @@
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	cout<<fixed<<setprecision(2);
+	string s;
+	while(cin>>s){
+		stringstream ss(s);
+		long long n,d;
+		char c;
+		ss>>n>>c>>d;
+		n=n*100+d;
+		if(n<=100*100)n=n*9+8*1000;
+		else if(n<=500*100)n*=8;
+		else n*=6;
+		long long a=n/1000,b=n%1000/10;
+		cout<<"$"<<a<<"."<<(b<10?"0":"")<<b<<endl;
+	}
+}
