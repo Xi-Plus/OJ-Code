aa1399fcf31e335f865ca3fce05b52dda22de421
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-15 20:37:28 +0800

    zj a450 AC

---
+++ ZJ/zj a450.cpp
@@ -0,0 +1,24 @@
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	int n,q;
+	cin>>n>>q;
+	vector<int> v;
+	int t;
+	while(n--){
+		cin>>t;
+		v.push_back(t);
+	}
+	sort(v.begin(),v.end());
+	int a,b;
+	while(q--){
+		cin>>a>>b;
+		int ans=upper_bound(v.begin(),v.end(),b)-lower_bound(v.begin(),v.end(),a);
+		if(ans)cout<<ans<<endl;
+		else cout<<"The candies are too short"<<endl;
+	}
+}
