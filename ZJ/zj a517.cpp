c59ef4892f42dcb098a870bb142a0e4515b02736
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-15 22:37:54 +0800

    zj a517 AC

--- ZJ/zj a517.cpp
+++ ZJ/zj a517.cpp
@@ -6,19 +6,11 @@ typedef unsigned long long ULL;
 int main(){
 //	ios::sync_with_stdio(false);
 //	cin.tie(0);
-	unsigned long long a,b,c;
-	while(cin>>a>>b>>c){
-		__int128 d=a,e=b,f=c;
-		__int128 sum=d*d+e*e+f*f;
-		__int128 l=0,r=9223372036854775807LL;
-		r*=4;
-		while(l<=r){
-			__int128 m=(l+r)/2;
-			if(m*m==sum){
-				cout<<(ULL)m<<endl;
-			}
-			if(m*m<sum)l=m+1;
-			else r=m-1;
-		}
+	unsigned long long v[3],m,n;
+	while(cin>>v[0]>>v[1]>>v[2]){
+		sort(v,v+3);
+		m=sqrt(v[1]-v[0]);
+		n=sqrt(v[2]-v[0]);
+		cout<<m*m+m*n+n*n<<endl;
 	}
 }
