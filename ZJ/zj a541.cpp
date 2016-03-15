176fa2b4db8efb3d6898bc3e4ad9a0dbbcf763be
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-15 22:42:16 +0800

    zj a5141 AC

---
+++ ZJ/zj a541.cpp
@@ -0,0 +1,23 @@
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	int n;
+	cin>>n;
+	map<string,bool> v;
+	string s;
+	while(n--){
+		cin>>s;
+		v[s]=true;
+	}
+	cin>>n;
+	while(n--){
+		cin>>s;
+		if(v[s])cout<<"yes"<<endl;
+		else cout<<"no"<<endl;
+		v[s]=true;
+	}
+}
