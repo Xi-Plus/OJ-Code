fc7cba7f1884ca0f31cf54f64cde4202e3087cca
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-15 14:44:34 +0800

    toj 306 update prob & remove debug code

--- TOJ/toj 306.cpp
+++ TOJ/toj 306.cpp
@@ -14,9 +14,8 @@ struct edge{
 };
 vector<edge> g[MAX_V];
 void insert_edge(int from,int to,double cost){
-//    cout<<from<<" "<<to<<" "<<cost<<endl;
     g[from].push_back({to,cost});  
-    g[to].push_back({from,cost}); /*if bidirected*/
+    g[to].push_back({from,cost});
 }
 struct pointd{
     int p;
@@ -44,70 +43,37 @@ int main(){
 //    ios::sync_with_stdio(false);
 //    cin.tie(0);
     int a,b,n,ta,tb;
+    int MAX_P;
     double tc;
-    cin>>a>>b>>n;
+    cin>>MAX_P>>n>>a>>b;
     vector<int> v;
     for(int q=0;q<n;q++){
         cin>>ta>>tb>>tc;
-        v.push_back(ta);
-        v.push_back(tb);
+        insert_edge(ta,tb,tc);
         l.push_back({ta,tb,tc});
-    }
-    sort(v.begin(),v.end());
-//    for(int q=0;q<v.size();q++){
-//        cout<<q<<" "<<v[q]<<endl;
-//    }
-//    cout<<endl;
-    vector<int>::iterator it;
-    it=unique(v.begin(),v.end());
-    v.resize(distance(v.begin(),it));
-    int MAX_P=v.size();
-//    for(int q=0;q<v.size();q++){
-//        cout<<q<<" "<<v[q]<<endl;
-//    }
-    a=lower_bound(v.begin(),v.end(),a)-v.begin()+1;
-    b=lower_bound(v.begin(),v.end(),b)-v.begin()+1;
-    for(int q=0;q<n;q++){
-        insert_edge(
-            lower_bound(v.begin(),v.end(),l[q].a)-v.begin()+1,
-            lower_bound(v.begin(),v.end(),l[q].b)-v.begin()+1,
-            l[q].cost
-        );
     }
     pointd temp;
     ppq.push({a,0});
     p[a].costa=0;
-//    bool update=true;
     while(!ppq.empty()){
         temp=ppq.top();
         ppq.pop();
-//        cout<<"find "<<temp.p<<" "<<temp.cost<<endl;
-//        update=false;
         for(int q=0;q<g[temp.p].size();q++){
-//            cout<<"check "<<g[temp.p][q].to<<" "<<g[temp.p][q].cost<<endl;
             if(temp.cost+g[temp.p][q].cost<p[g[temp.p][q].to].costa){
                 p[g[temp.p][q].to].costa=temp.cost+g[temp.p][q].cost;
-//                cout<<"update "<<g[temp.p][q].to<<" "<<p[g[temp.p][q].to].costa<<endl;
                 ppq.push({g[temp.p][q].to,p[g[temp.p][q].to].costa});
-//                update=true;
             }
         }
     }
     ppq.push({b,0});
     p[b].costb=0;
-//    bool update=true;
     while(!ppq.empty()){
         temp=ppq.top();
         ppq.pop();
-//        cout<<"find "<<temp.p<<" "<<temp.cost<<endl;
-//        update=false;
         for(int q=0;q<g[temp.p].size();q++){
-//            cout<<"check "<<g[temp.p][q].to<<" "<<g[temp.p][q].cost<<endl;
             if(temp.cost+g[temp.p][q].cost<p[g[temp.p][q].to].costb){
                 p[g[temp.p][q].to].costb=temp.cost+g[temp.p][q].cost;
-//                cout<<"update "<<g[temp.p][q].to<<" "<<p[g[temp.p][q].to].costa<<endl;
                 ppq.push({g[temp.p][q].to,p[g[temp.p][q].to].costb});
-//                update=true;
             }
         }
     }
@@ -142,7 +108,6 @@ int main(){
         }
     }
     for(int q=1;q<=MAX_P;q++){
-//        cout<<q<<" "<<p[q].costa<<" "<<p[q].costb<<endl;
         if(p[q].costa==p[q].costb){
             ans.push_back({q,q,0});
         }
