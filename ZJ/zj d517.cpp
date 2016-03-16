4bd0feafc4b2e7f77f9dd67157235db31f752893
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-16 09:52:00 +0800

    zj d517 AC

---
+++ ZJ/zj d517.cpp
@@ -0,0 +1,22 @@
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	int t;
+	string s;
+	while(cin>>t){
+		map<string,int> v;
+		int n=0;
+		while(t--){
+			cin>>s;
+			if(v[s]) cout<<"Old! "<<v[s]<<endl;
+			else {
+				v[s]=++n;
+				cout<<"New! "<<n<<endl;
+			}
+		}
+	}
+}
