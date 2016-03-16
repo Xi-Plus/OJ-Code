3e9137c87a55547a77f9f2f82906857700bfd657
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-16 09:56:27 +0800

    zj d549 AC

---
+++ ZJ/zj d549.cpp
@@ -0,0 +1,12 @@
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	long double a,b,c;
+	while(cin>>a>>b>>c){
+		cout<<fixed<<setprecision(2)<<sqrt(a*a-b*b+c*c)<<endl;
+	}
+}
