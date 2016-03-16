c7556a30524647bdcc14b46afacf7cdaa2f351ba
xiplus <huangxuanyuxiplus@gmail.com>
2016-03-16 10:11:21 +0800

    zj d712 TLE

---
+++ ZJ/zj d712.cpp
@@ -0,0 +1,23 @@
+// By KRT girl xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int count(int n){
+	if(n==1) return 0;
+	if(n&1) return count(n*3+1)+1;
+	return count(n/2)+1;
+}
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	int a,b;
+	while(cin>>a>>b){
+		cout<<a<<" "<<b<<" ";
+		if(a>b)swap(a,b);
+		int ans=0;
+		for(int q=a;q<=b;q++){
+			ans=max(count(q)+1,ans);
+		}
+		cout<<ans<<endl;
+	}
+}
