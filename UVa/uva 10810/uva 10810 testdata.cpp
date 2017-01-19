a46866e93c85b0c78983141a8307375f3b67c0a6
xiplus <huangxuanyuxiplus@gmail.com>
2017-01-19 14:23:07 +0800

    +uva 10810 testdata

---
+++ UVa/uva 10810/uva 10810 testdata.cpp
@@ -0,0 +1,16 @@
+// By xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main(){
+//	ios::sync_with_stdio(false);
+//	cin.tie(0);
+	srand(time(NULL));
+	int n=100000;
+	cout<<n<<endl;
+	for(int q=0;q<n;q++){
+		cout<<rand()%10000<<endl;
+	}
+	cout<<0<<endl;
+}
+
