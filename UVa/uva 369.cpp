98ed761d8ce32372a91c7321c056e234aa42d4f8
xiplus <huangxuanyuxiplus@gmail.com>
2019-01-24 18:49:33 +0800

    Update uva 369.cpp

--- UVa/uva 369.cpp
+++ UVa/uva 369.cpp
@@ -8,19 +8,8 @@ int main() {
 	while(cin>>n>>m, n) {
 		mm = m;
 		if(m*2 > n) m = n-m;
-		if(m<5) {
-			long long ans = 1;
-			for(int q=1; q<=m; q++) {
-				ans *= n-q+1;
-			} 
-			for(int q=1; q<=m; q++) {
-				ans /= q;
-			}
-			cout<<n<<" things taken "<<mm<<" at a time is "<<ans<<" exactly."<<endl;
-			continue;
-		}
-		long long ans = n*(n-1)*(n-2)*(n-3)*(n-4)/2/3/4/5;
-		for(int q=6; q<=m; q++) {
+		long long ans = 1;
+		for(int q=1; q<=m; q++) {
 			ans *= n-q+1;
 			ans /= q;
 		}
