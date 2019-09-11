33f01986721279df54ad6a813ac38084b87140dd
xiplus <huangxuanyuxiplus@gmail.com>
2019-09-11 20:37:07 +0800

    Create uva 10071.cpp

---
+++ UVa/uva 10071.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int v, t;
+	while(cin>>v>>t){
+		cout<<2*v*t<<endl;
+	}
+}
