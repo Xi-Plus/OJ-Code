e8f12e831892b126b3537a1ffd8aee6bdf199d34
xiplus <huangxuanyuxiplus@gmail.com>
2019-10-29 11:42:33 +0800

    Update leetcode 442.py

--- LeetCode/leetcode 442.py
+++ LeetCode/leetcode 442.py
@@ -1,12 +1,11 @@
 class Solution:
     def findDuplicates(self, nums):
         dic = {}
+        ans = []
         for v in nums:
             if v not in dic:
                 dic[v] = 0
             dic[v] += 1
-        ans = []
-        for v in dic:
-            if dic[v] >= 2:
+            if dic[v] == 2:
                 ans.append(v)
         return ans
