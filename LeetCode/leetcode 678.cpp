127c59793e9af95ae7ad21f3833e948bf8de85c0
xiplus <huangxuanyuxiplus@gmail.com>
2020-08-17 11:01:58 +0800

    Add some comments

--- LeetCode/leetcode 678.cpp
+++ LeetCode/leetcode 678.cpp
@@ -5,8 +5,10 @@ using namespace std;
 /*
 "" => 0
 "*" => 1
+"**" => 2
 "*("  => 1, 0
 "*(*" => 1, 1
+"*(**" => 1, 2
 "*(*(" => 1, 1, 0
 */
 class Solution {
@@ -18,16 +20,21 @@ class Solution {
 
 		for (int q = 0; q < s.size(); q++) {
 			if (s[q] == '(') {
+				// 新增一個項目
 				idx++;
 				cnt.push_back(0);
 			} else if (s[q] == ')') {
 				if (idx == 0) {
+					// 沒有剩餘的 (
 					if (cnt[0] == 0) {
+						// 沒有剩餘的 *
 						return false;
 					} else {
+						//消耗剩餘的 *
 						cnt[0]--;
 					}
 				} else {
+					// 移除最右邊的 ( ，合併最後兩項
 					cnt[idx - 1] += cnt[idx];
 					cnt.pop_back();
 					idx--;
@@ -39,8 +46,10 @@ class Solution {
 
 		while (idx > 0) {
 			if (cnt[idx] == 0) {
+				// ( 右邊沒有 * 可供配對
 				return false;
 			}
+			// 消耗一個 * 來移除一個 (
 			cnt[idx - 1] += cnt[idx] - 1;
 			cnt.pop_back();
 			idx--;
