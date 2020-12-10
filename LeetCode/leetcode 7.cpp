f0c62ad43c2fe9922638a4efd3de0b96d42e92f9
xiplus <huangxuanyuxiplus@gmail.com>
2020-12-10 10:35:37 +0800

    Update leetcode 7.cpp

--- LeetCode/leetcode 7.cpp
+++ LeetCode/leetcode 7.cpp
@@ -4,12 +4,13 @@ using namespace std;
 class Solution {
    public:
 	int reverse(int x) {
-		string s = to_string(x);
+		long long x2 = x;
 		string n = "";
-		if (s[0] == '-') {
+		if (x2 < 0) {
 			n = "-";
-			s = s.substr(1);
+			x2 = -x2;
 		}
+		string s = to_string(x2);
 		std::reverse(s.begin(), s.end());
 		long long ans = stoll(n + s);
 		if (ans < -2147483648LL || ans > 2147483647LL) {
