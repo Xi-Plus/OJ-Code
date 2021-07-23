966ff1e74ccf5687f587e2c6a4e6b58294ec3f7c
xiplus <huangxuanyuxiplus@gmail.com>
2021-07-23 18:56:20 +0800

    Update toj 123-2.cpp

--- TOJ/toj 123-2.cpp
+++ TOJ/toj 123-2.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 int main() {
-	int n;
-	std::cin >> n;
-	char a[200010] = {'1'};	 //  left padding for first line
-	scanf("%s %s", &a[1], &a[n + 3]);
-	memset(&a[n + 1], '1', 2);	// right padding for first line and left padding for second line
-	a[n * 2 + 3] = '1';			// right padding for second line
-	for (int i = 0; i < n * 2 + 4; i++) {
+	char a[200010];
+	scanf("%d", (int*)&a[200005]);
+	scanf("%s %s", &a[1], &a[*(int*)&a[200005] + 3]);
+	a[0] = a[*(int*)&a[200005] + 1] = a[*(int*)&a[200005] + 2] = a[*(int*)&a[200005] * 2 + 3] = '1';  // padding
+	for (int i = 0; i < *(int*)&a[200005] * 2 + 4; i++) {
 		a[i] -= '0';
 	}
 	bool ok = true;
-	for (int i = 1; i <= n; i++) {
-		ok &= (a[i] | (a[i + n + 1] & a[i + n + 2] & a[i + n + 3]));
+	for (int i = 1; i <= *(int*)&a[200005]; i++) {
+		ok &= (a[i] | (a[i + *(int*)&a[200005] + 1] & a[i + *(int*)&a[200005] + 2] & a[i + *(int*)&a[200005] + 3]));
 	}
 	std::cout << (ok ? "FENDED OFF!" : "FENESTRATION FORTIFICATION FAILURE!") << std::endl;
 }
