afcc5ef6b1942f86da7ac27f00ab9b1cbcc0045c
xiplus <huangxuanyuxiplus@gmail.com>
2021-07-23 18:32:04 +0800

    Update toj 123-2.cpp

--- TOJ/toj 123-2.cpp
+++ TOJ/toj 123-2.cpp
@@ -1,28 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <bits/stdc++.h>
 int main() {
 	int n;
-	cin >> n;
-	char a[2][n + 2];
-	a[0][0] = 1;
-	a[1][0] = 1;
-	a[0][n + 1] = 1;
-	a[1][n + 1] = 1;
-	for (int q = 1; q <= n; q++) {
-		cin >> a[0][q];
-		a[0][q] -= '0';
-	}
-	for (int q = 1; q <= n; q++) {
-		cin >> a[1][q];
-		a[1][q] -= '0';
+	std::cin >> n;
+	char a[200010] = {'1'};	 //  left padding for first line
+	scanf("%s %s", &a[1], &a[n + 3]);
+	memset(&a[n + 1], '1', 2);	// right padding for first line and left padding for second line
+	a[n * 2 + 3] = '1';			// right padding for second line
+	for (int i = 0; i < n * 2 + 4; i++) {
+		a[i] -= '0';
 	}
 	bool ok = true;
-	for (int q = 1; q <= n; q++) {
-		ok &= (a[0][q] | (a[1][q - 1] & a[1][q] & a[1][q + 1]));
+	for (int i = 1; i <= n; i++) {
+		ok &= (a[i] | (a[i + n + 1] & a[i + n + 2] & a[i + n + 3]));
 	}
-	if (ok) {
-		cout << "FENDED OFF!" << endl;
-	} else {
-		cout << "FENESTRATION FORTIFICATION FAILURE!" << endl;
-	}
+	std::cout << (ok ? "FENDED OFF!" : "FENESTRATION FORTIFICATION FAILURE!") << std::endl;
 }
