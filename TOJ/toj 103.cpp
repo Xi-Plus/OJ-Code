da92be404b35b2aac37e112591fcf2bf17c455f2
xiplus <huangxuanyuxiplus@gmail.com>
2021-07-24 12:32:56 +0800

    Update toj 103.cpp

--- TOJ/toj 103.cpp
+++ TOJ/toj 103.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
-int main(){
-    string a,b;
-    int c,d;
-    cin>>a>>c>>b>>d;
-    if(a==b&&c==d)cout<<"GOOD"<<endl;
-    else if(a!=b&&c!=d)cout<<"OTZ"<<endl;
-    else cout<<"=~="<<endl;
+int main() {
+	string a, b, c, d;
+	cin >> a >> c >> b >> d;
+	if (a == b && c == d) {
+		cout << "GOOD" << endl;
+	} else if (a != b && c != d) {
+		cout << "OTZ" << endl;
+	} else {
+		cout << "=~=" << endl;
+	}
 }
