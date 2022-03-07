5a3a5db0536f65a008a1424ec8f3a83735ca9054
xiplus <huangxuanyuxiplus@gmail.com>
2022-03-07 11:55:25 +0800

    Create leetcode 799.py

---
+++ LeetCode/leetcode 799.py
@@ -0,0 +1,12 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        ans = [[0 for i in range(query_row + 2)] for j in range(query_row + 2)]
+        ans[0][0] += poured
+        for i in range(0, query_row + 1):
+            for j in range(i + 1):
+                if ans[i][j] > 1:
+                    out = ans[i][j] - 1
+                    ans[i + 1][j] += out / 2
+                    ans[i + 1][j + 1] += out / 2
+                    ans[i][j] = 1
+        return ans[query_row][query_glass]
