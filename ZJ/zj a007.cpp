b9f612f313de560a77aba87eb7db9e6006460def
xiplus <huangxuanyuxiplus@gmail.com>
2024-07-27 10:44:39 +0800

    Update zj a007.cpp

--- ZJ/zj a007.cpp
+++ ZJ/zj a007.cpp
@@ -1,42 +1,47 @@
+// By xiplus
 #include <bits/stdc++.h>
 #define endl '\n'
 using namespace std;
-bool nisp[46341]={false};
+bool nisp[46341] = {false};
 int p[10000];
-int main(){
-//	ios::sync_with_stdio(0);
-//	cin.tie(0);
-	nisp[0]=nisp[1]=true;
-	int i=2,j,k=0;
-	while(i<46341){
-		if(!nisp[i]){
-			p[k++]=i;
-			j=i*2;
-			while(j<46341){
-				nisp[j]=true;
-				j+=i;
+int main() {
+	// ios::sync_with_stdio(0); cin.tie(0);
+	nisp[0] = nisp[1] = true;
+	int i = 2, j, k = 0;
+	while (i < 46341) {
+		if (!nisp[i]) {
+			p[k++] = i;
+			j = i * 2;
+			while (j < 46341) {
+				nisp[j] = true;
+				j += i;
 			}
 		}
 		i++;
 	}
 	int n;
-	while(cin>>n){
-		if(n==2||n==3)cout<<"½è¼Æ"<<endl;
-		else if((n-1)%6&&(n+1)%6)cout<<"«D½è¼Æ"<<endl;
-		else if(n<=46340){
-			if(binary_search(p,p+k,n))cout<<"½è¼Æ"<<endl;
-			else cout<<"«D½è¼Æ"<<endl;
-		}
-		else{
-			bool x=0;
-			for(int q=0;q<k&&p[q]*p[q]<=n;q++){
-				if(n%p[q]==0){
-					x=1;
+	while (cin >> n) {
+		if (n == 2 || n == 3)
+			cout << "è³ªæ•¸" << endl;
+		else if ((n - 1) % 6 && (n + 1) % 6)
+			cout << "éžè³ªæ•¸" << endl;
+		else if (n <= 46340) {
+			if (binary_search(p, p + k, n))
+				cout << "è³ªæ•¸" << endl;
+			else
+				cout << "éžè³ªæ•¸" << endl;
+		} else {
+			bool x = 0;
+			for (int q = 0; q < k && p[q] * p[q] <= n; q++) {
+				if (n % p[q] == 0) {
+					x = 1;
 					break;
 				}
 			}
-			if(x)cout<<"«D½è¼Æ"<<endl;
-			else cout<<"½è¼Æ"<<endl;
+			if (x)
+				cout << "éžè³ªæ•¸" << endl;
+			else
+				cout << "è³ªæ•¸" << endl;
 		}
 	}
 }
