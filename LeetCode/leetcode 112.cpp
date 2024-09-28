d1d7df02e65351947f6d144974aa3078cafa67f9
xiplus <huangxuanyuxiplus@gmail.com>
2024-09-28 12:51:42 +0800

    Create leetcode 112.cpp

---
+++ LeetCode/leetcode 112.cpp
@@ -0,0 +1,31 @@
+// By xiplus
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+   public:
+	bool hasPathSum(TreeNode* root, int targetSum) {
+		if (!root) {
+			return false;
+		}
+		if (!root->left && !root->right) {
+			return root->val == targetSum;
+		}
+		bool ans = false;
+		if (root->left) {
+			ans |= hasPathSum(root->left, targetSum - root->val);
+		}
+		if (root->right) {
+			ans |= hasPathSum(root->right, targetSum - root->val);
+		}
+		return ans;
+	}
+};
