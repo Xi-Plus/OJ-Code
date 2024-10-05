30b669c3f7b83e311c46506c6315f874e8b13934
xiplus <huangxuanyuxiplus@gmail.com>
2024-10-05 15:48:36 +0800

    Update leetcode 101.cpp

--- LeetCode/leetcode 101.cpp
+++ LeetCode/leetcode 101.cpp
@@ -16,15 +16,15 @@ class Solution {
 		if (!root) {
 			return true;
 		}
-		return dfs(root->left, root->right);
+		return isSymmetric(root->left, root->right);
 	}
-	bool dfs(TreeNode *p, TreeNode *q) {
+	bool isSymmetric(TreeNode *p, TreeNode *q) {
 		if (!p && !q) {
 			return true;
 		}
 		if (!p || !q || p->val != q->val) {
 			return false;
 		}
-		return dfs(p->left, q->right) && dfs(p->right, q->left);
+		return isSymmetric(p->left, q->right) && isSymmetric(p->right, q->left);
 	}
 };
