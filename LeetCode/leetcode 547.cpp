cc2896207f867e7dd6ae9a691527860d67410c8a
xiplus <huangxuanyuxiplus@gmail.com>
2024-10-26 12:10:25 +0800

    Create leetcode 547.cpp

---
+++ LeetCode/leetcode 547.cpp
@@ -0,0 +1,66 @@
+// By xiplus
+class Solution {
+   public:
+	int findCircleNum(vector<vector<int>>& isConnected) {
+		return disjointSet(isConnected);
+		// return dfs(isConnected);
+	}
+	int dfs(vector<vector<int>>& isConnected) {
+		int n = isConnected.size();
+		vector<int> edges[n];
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (isConnected[i][j] == 1) {
+					edges[i].push_back(j);
+					edges[j].push_back(i);
+				}
+			}
+		}
+		vector<bool> visited(n, false);
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
+				ans++;
+				dfsCore(edges, visited, i);
+			}
+		}
+		return ans;
+	}
+	void dfsCore(vector<int> edges[], vector<bool>& visited, int x) {
+		if (visited[x]) {
+			return;
+		}
+		visited[x] = true;
+		for (auto y : edges[x]) {
+			dfsCore(edges, visited, y);
+		}
+	}
+	int disjointSet(vector<vector<int>>& isConnected) {
+		int n = isConnected.size();
+		vector<int> parent(n);
+		for (int i = 0; i < n; i++) {
+			parent[i] = i;
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (isConnected[i][j] == 1) {
+					parent[findParent(parent, j)] = findParent(parent, i);
+				}
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			parent[i] = findParent(parent, i);
+			if (parent[i] == i) {
+				ans++;
+			}
+		}
+		return ans;
+	}
+	int findParent(vector<int>& parent, int x) {
+		if (parent[x] == x) {
+			return x;
+		}
+		return parent[x] = findParent(parent, parent[x]);
+	}
+};
