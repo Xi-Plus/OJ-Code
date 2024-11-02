6ce62443e527c0d8085e5a5d6b2ab25f609e6345
xiplus <huangxuanyuxiplus@gmail.com>
2024-11-02 15:48:39 +0800

    Update zj d636.cpp

--- ZJ/zj d636.cpp
+++ ZJ/zj d636.cpp
@@ -2,21 +2,22 @@
 #include <bits/stdc++.h>
 #define endl '\n'
 using namespace std;
-const long long m=10007;
-long long power(long long a,long long b){
-	if(b==1) return a;
-	if(b&1){
-		return power(a,b/2)%m*power(a,b/2)%m*a%m;
-	}else {
-		return power(a,b/2)%m*power(a,b/2)%m;
+const long long m = 10007;
+long long power(long long a, long long b) {
+	if (b == 1) {
+		return a;
+	}
+	long long temp = power(a, b / 2) % m;
+	if (b & 1) {
+		return temp * temp * a % m;
+	} else {
+		return temp * temp;
 	}
 }
-int main(){
-//	ios::sync_with_stdio(false);
-//	cin.tie(0);
-	long long a,b;
-	while(cin>>a>>b){
-		cout<<power(a,b)<<endl;
+int main() {
+	// ios::sync_with_stdio(false); cin.tie(0);
+	long long a, b;
+	while (cin >> a >> b) {
+		cout << power(a, b) << endl;
 	}
 }
-
