3eb37e306f3ffd0b8025e4d66a2582d5893ee2de
xiplus <huangxuanyuxiplus@gmail.com>
2025-05-30 15:20:04 +0800

    Update leetcode 1534.cpp

--- LeetCode/leetcode 1534.cpp
+++ LeetCode/leetcode 1534.cpp
@@ -2,6 +2,11 @@
 class Solution {
    public:
 	int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+		return solve1(arr, a, b, c);
+		// return solve2(arr, a, b, c);
+	}
+
+	int solve1(vector<int>& arr, int a, int b, int c) {
 		int ans = 0;
 		for (int i = 0; i < arr.size() - 2; i++) {
 			for (int j = i + 1; j < arr.size() - 1; j++) {
@@ -10,6 +15,20 @@ class Solution {
 						if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
 							ans++;
 						}
+					}
+				}
+			}
+		}
+		return ans;
+	}
+
+	int solve2(vector<int>& arr, int a, int b, int c) {
+		int ans = 0;
+		for (int i = 0; i < arr.size() - 2; i++) {
+			for (int j = i + 1; j < arr.size() - 1; j++) {
+				for (int k = j + 1; k < arr.size(); k++) {
+					if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) {
+						ans++;
 					}
 				}
 			}
