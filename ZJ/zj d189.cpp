fa754c69f6e2b147b07cc2dd32836d96f698bc6a
xiplus <huangxuanyuxiplus@gmail.com>
2025-06-01 15:30:03 +0800

    Create zj d189.cpp

---
+++ ZJ/zj d189.cpp
@@ -0,0 +1,12 @@
+// By xiplus
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	// ios::sync_with_stdio(false); cin.tie(0);
+	int n;
+	while (cin >> n) {
+		cout << n * 3 / 2 << endl;
+	}
+}
