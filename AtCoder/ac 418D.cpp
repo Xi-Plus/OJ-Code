73a83f8ad4a31c85761d98762e1be1a878aa2ad4
xiplus <huangxuanyuxiplus@gmail.com>
2025-08-16 11:41:22 +0800

    Create ac 418D.cpp

---
+++ AtCoder/ac 418D.cpp
@@ -0,0 +1,27 @@
+// By xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	// ios::sync_with_stdio(false); cin.tie(0);
+	int n;
+	string s;
+	cin >> n >> s;
+	int dp[n][2] = {};
+	dp[0][s[0] == '1'] = 1;
+	for (int i = 1; i < n; i++) {
+		if (s[i] == '0') {
+			dp[i][0] = dp[i - 1][1] + 1;
+			dp[i][1] = dp[i - 1][0];
+		} else {
+			dp[i][0] = dp[i - 1][0];
+			dp[i][1] = dp[i - 1][1] + 1;
+		}
+	}
+	long long ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans += dp[i][1];
+	}
+	cout << ans << endl;
+}
