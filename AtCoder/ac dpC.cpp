65d21c14818b25f3d890580b35dead9fb6417912
xiplus <huangxuanyuxiplus@gmail.com>
2025-09-13 12:37:40 +0800

    Create ac dpC.cpp

---
+++ AtCoder/ac dpC.cpp
@@ -0,0 +1,24 @@
+// By xiplus
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	// ios::sync_with_stdio(false); cin.tie(0);
+	int n;
+	cin >> n;
+	int a[n][3];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i][0] >> a[i][1] >> a[i][2];
+	}
+	int dp[n][3];
+	dp[0][0] = a[0][0];
+	dp[0][1] = a[0][1];
+	dp[0][2] = a[0][2];
+	for (int i = 1; i < n; i++) {
+		dp[i][0] = a[i][0] + max(dp[i - 1][1], dp[i - 1][2]);
+		dp[i][1] = a[i][1] + max(dp[i - 1][0], dp[i - 1][2]);
+		dp[i][2] = a[i][2] + max(dp[i - 1][0], dp[i - 1][1]);
+	}
+	cout << max(dp[n - 1][0], max(dp[n - 1][1], dp[n - 1][2])) << endl;
+}
