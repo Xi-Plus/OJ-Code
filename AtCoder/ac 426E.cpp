4adbd05619720ad55a01c64f23b3ae158ffff742
xiplus <huangxuanyuxiplus@gmail.com>
2025-10-25 11:26:43 +0800

    Update ac 426E.cpp

--- AtCoder/ac 426E.cpp
+++ AtCoder/ac 426E.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 long double tsx, tsy, tgx, tgy, asx, asy, agx, agy;
-long double tdis, adis, maxdis;
+long double tdis, adis;
 long double tx, ty, ax, ay;
 long double f(long double t) {
 	if (t >= tdis) {
@@ -32,9 +32,8 @@ int main() {
 		cin >> tsx >> tsy >> tgx >> tgy >> asx >> asy >> agx >> agy;
 		tdis = sqrt(pow(tsx - tgx, 2) + pow(tsy - tgy, 2));
 		adis = sqrt(pow(asx - agx, 2) + pow(asy - agy, 2));
-		maxdis = max(tdis, adis);
-		long double l = 0, r = maxdis, ml, mr, ansl, ansr;
-		for (int i = 0; i < 1000; i++) {
+		long double l = 0, r = min(tdis, adis), ml, mr, ansl, ansr, ans1;
+		for (int i = 0; i < 100; i++) {
 			ml = (l * 2 + r) / 3;
 			mr = (l + r * 2) / 3;
 			ansl = f(ml);
@@ -49,7 +48,24 @@ int main() {
 				break;
 			}
 		}
-		assert(!isnan(ansl));
-		cout << fixed << setprecision(30) << sqrt(ansl) << endl;
+		ans1 = ansl;
+
+		l = min(tdis, adis), r = max(tdis, adis);
+		for (int i = 0; i < 100; i++) {
+			ml = (l * 2 + r) / 3;
+			mr = (l + r * 2) / 3;
+			ansl = f(ml);
+			ansr = f(mr);
+			if (ansl < ansr) {
+				r = mr;
+			} else {
+				l = ml;
+			}
+			if (abs(l - r) < 1e-15 && abs(ansl - ansr) < 1e-15) {
+				// cout << i << endl;
+				break;
+			}
+		}
+		cout << fixed << setprecision(30) << sqrt(min(ans1, ansl)) << endl;
 	}
 }
