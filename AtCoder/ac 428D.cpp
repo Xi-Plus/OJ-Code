78bc6b74d3147a0a67e40beef29571d6c49b9bb4
xiplus <huangxuanyuxiplus@gmail.com>
2025-10-25 11:18:13 +0800

    Update ac 428D.cpp

--- AtCoder/ac 428D.cpp
+++ AtCoder/ac 428D.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 vector<long long> p10;
+long long sqrt2(long long x) {
+	long long y = sqrt(x);
+	while (y * y > x) {
+		y--;
+	}
+	while ((y + 1) * (y + 1) <= x) {
+		y++;
+	}
+	return y;
+}
 long long square_in_range(long long a, long long b) {
-	// cout << "check " << a << " " << b << endl;
-	if (b < a) {
-		return 0;
-	}
-	long long sqrta = sqrt(a);
-	long long sqrtb = sqrt(b);
-	if (sqrta * sqrta == a) {
-		return sqrtb - sqrta + 1;
-	} else {
-		return sqrtb - sqrta;
-	}
+	return sqrt2(b) - sqrt2(a - 1);
 }
 long long concat(long long a, long long b) {
 	return stoll(to_string(a) + to_string(b));
@@ -25,9 +25,7 @@ void sol() {
 	cin >> c >> d;
 	long long ans = 0;
 	for (int dig = to_string(c).size(); dig <= to_string(c + d).size(); dig++) {
-		// cout << "dig " << dig << endl;
 		ans += square_in_range(concat(c, p10[dig - 1]), concat(c, p10[dig] - 1));
-		// cout << "ans " << ans << endl;
 	}
 	ans -= square_in_range(concat(c, p10[to_string(c).size() - 1]), concat(c, c));
 	ans -= square_in_range(concat(c, c + d) + 1, concat(c, p10[to_string(c + d).size()] - 1));
